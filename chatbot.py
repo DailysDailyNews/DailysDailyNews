d68382998e7c0e51f43539b6564f803a77948180
Daily Investors <204614137+DailysDailyNews@users.noreply.github.com>
2025-05-17 11:18:00 -0700

    Basic Self-Learning Chat News Bot
    1.  Must Have Python
    2. import tkinter as tk
    3.  chmod File
    4. Run file
    5. Looking for sponsors to help my education, studying, and developing.
    6. Will do advertising on my sites, promote businesses, pen test, add businesses in LISCENSE and READMEs. Use your programs in my tools/sites
    7. Appreciate any and all donations. Thank you and God Bless

---
+++ chatbot.py
@@ -0,0 +1,46 @@
+def chatbot():
+  knowledge = {
+    "hello": "Hi there!",
+    "how are you": "i'm doing well, thank you!",
+    "What is your name?": "I am a chatbot created by Daily Investors",
+    "What is your purpose?": "To help you better understand",
+    "What is your investment strategy?": "I don't have a specific investment strategy, but I can provide information on various techniques.",
+    "Help": "I can help, with future funds the News will continue in the future.",
+    "Bye": "Goodbye!"
+  }
+  
+def respond(message):
+  message = message.lower()
+  if message in knowledge:
+    return knowledge[message]
+  else:
+    return "I'm sorry, havnt been sponsored yet. Will be soon!"
+  
+  def learn(question, answer):
+    knowledge[question.lower()] = answer
+    print(f"Thanks! I've learned that when you say '{question}', I should respond with '{answer}'.")
+
+    print("Hello! I', a simple learning chatbot. Type 'bye' to exit.")
+    while True:
+      user_input = input(".")
+      if user_input.lower() == 'bye':
+        print(respond(user_input))
+        break
+      else:
+        response = respond(user_input)
+        break
+    else:
+      response = respond(user_input)
+      print(response)
+      if "Learn": in user_input:
+question = input("What would you like to teach me?")
+      answer = input("Stock Market Strategies, Updated News, Future Picks")
+      learn(question, answer)
+      print("Thank You, we value education and will continue to learn.")
+    else:
+      new_answere = input("What should I say?")
+      learn(user_input, new_answere)
+      
+
+if __name__ == "__main__":
+   chatbot()
